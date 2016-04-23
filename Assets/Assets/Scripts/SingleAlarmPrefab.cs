83acde5ff9e095810e518fec94878b8a8f754808
Donal O Domhnaill <donalod@gmail.com>
2016-04-23 19:34:43 +0100

    Committing latest changes.. which are not many

--- Assets/Assets/Scripts/SingleAlarmPrefab.cs
+++ Assets/Assets/Scripts/SingleAlarmPrefab.cs
@@ -1,35 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SingleAlarm : MonoBehaviour {
-
-    /*
-    This class will do and contain the following:
-
-        1)  Contain a list of all the variables needed to save all the details of the alarm. These are:
-            *   Unixtime
-            *   Alarm name
-            *   Repeat weekly (yes/no)
-            *   sun (on/off)
-            *   mon (on/off)
-            *   tue (on/off)
-            *   wed (on/off)
-            *   thr (on/off)
-            *   fri (on/off)
-            *   sat (on/off)
-            *   volume
-            *   alarm tone (location of tone)
-            *   background image (image background)
-        2)  Here is the time line of how things will happen.
-            *   First from the Main Menu scene the "Create New" button is clicked. 
-            *   We are then taken to the Edit Alarm scene.
-            *   At the Edit Alarm scene the details contained in point 1) above will be recoreded simply as playerpref settings. 
-            *   When we are happy with the details that we have chosen then we click on the "Set Alarm" button. 
-            *   Clicking on the set alarm button takes us to the clock screen or the "Main Screen" scene (this is just a choice as we could have gone to Main Menu again)
-            *   Clicking on the set alarm button also creates the prefab for the single alarm just created.
-            *   The prefab just created or "instantiated" will now show up in the "Main Menu" scene. 
-        3)  There has to be a way to figure out which instance will be on top. This will be done using a unix timestamp
-     */
+public class SingleAlarmPrefab : MonoBehaviour {
 
     // Use this for initialization. Create the playerprefs here. 
     void Start () {
